3887524dae1b94ef456d1b2d1634ca59b51a890f
Marcello Dichiera <marcello_dichiera@hotmail.it>
2026-01-10 11:33:42 +0000

    testing groq models

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ from agno.agent import Agent
 from agno.tools.yfinance import YFinanceTools
 from agno.tools.tavily import TavilyTools
 from agno.models.openai import OpenAIChat
+from agno.models.groq import Groq
 from dotenv import load_dotenv
 from agents_instructions.fundamental_analysis_agent_instructions import FundamentalExpectedOutput, FundamentalInstructions
 
@@ -15,6 +16,7 @@ load_dotenv()
 def fundamental_analysis_agent():
     agent = Agent(
         model=OpenAIChat("gpt-5-mini"),
+        model=Groq(id="llama-3.3-70b-versatile"),
         description="You are a comprehensive fundamental investment analyst with access to financial data functions.",
         tools= [YFinanceTools(exclude_tools=["get_company_news", "get_technical_indicators"])],
         name="Fundamental Analysis Agent",
