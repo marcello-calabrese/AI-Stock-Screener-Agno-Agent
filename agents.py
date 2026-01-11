43de38c7ef3468e96c41d225f1deb7ebbbcf5e2e
Marcello Dichiera <marcello_dichiera@hotmail.it>
2026-01-11 13:58:51 +0000

    added memory to fund analysis agent

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ from agno.agent import Agent
 from agno.tools.yfinance import YFinanceTools
 from agno.tools.tavily import TavilyTools
 from agno.models.openai import OpenAIChat
+from agno.db.sqlite import SqliteDb
 from agno.models.groq import Groq
 from dotenv import load_dotenv
 from agents_instructions.fundamental_analysis_agent_instructions import FundamentalExpectedOutput, FundamentalInstructions
@@ -10,6 +11,9 @@ from agents_instructions.fundamental_analysis_agent_instructions import Fundamen
 # Load the environment variables
 
 load_dotenv()
+
+# Storage agent sessions in a SQLite DB
+storage = SqliteDb(db_file="tmp/agent_history.db")
 
 # Fundamental Analysis Agent with Yahoo Finance Tools
 
@@ -23,6 +27,12 @@ def fundamental_analysis_agent():
         instructions= FundamentalInstructions,
         expected_output= FundamentalExpectedOutput,
         markdown=True,
+        enable_user_memories=True,
+        add_history_to_context=True,
+        num_history_runs=3,
+        db=storage,
+        compress_tool_results=True,
+        stream=True,
         )
     return agent
 
